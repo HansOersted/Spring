c6260eb0ae5c56577fc79cf8eb75891302d499d3
Zhe Shen <80969277+HansOersted@users.noreply.github.com>
2024-04-11 16:29:45 +0900

    Add files via upload

---
+++ params.m
@@ -0,0 +1,12 @@
+% Define the parameters of the spring and the particle
+
+g = 9.81;
+m = 1.0;  % mass of the particle
+
+L0 = 1.0;  % initial length of the spring (nature length) 
+k = 10;  % spring constant
+beta = 2;  % damping constant
+
+
+initial_theta = 0.5;
+initial_L = 0;
